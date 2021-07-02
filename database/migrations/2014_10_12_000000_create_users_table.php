96d0a7e374bbb061e2925805e0f4309487ab4440
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-07-02 16:41:22 +0200

    users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('address');
+            $table->string('phone')->unique();
+            $table->string('website');
+            $table->string('company');
             $table->timestamps();
         });
     }
