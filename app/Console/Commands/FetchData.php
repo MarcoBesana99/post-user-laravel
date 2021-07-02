1fd95de80a17a337e85c879f2b2beb64ce832392
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-07-02 17:32:24 +0200

    schedule the fetch data command daily

--- app/Console/Commands/FetchData.php
+++ app/Console/Commands/FetchData.php
@@ -42,26 +42,26 @@ class FetchData extends Command
     {
         $res = Http::get('https://jsonplaceholder.typicode.com/users');
         $users = json_decode($res->getBody()->getContents(), true);
-        foreach($users as $user) {
+        foreach ($users as $user) {
             $u = new User();
-            $u->id = $user['id']; 
-            $u->name = $user['name']; 
-            $u->username = $user['username']; 
-            $u->email = $user['email']; 
-            $u->address = $user['address']['street'] . ', ' . $user['address']['suite'] . ', ' . $user['address']['city'] . ', ' . $user['address']['zipcode']; 
-            $u->phone = $user['phone']; 
-            $u->website = $user['website']; 
-            $u->company = $user['company']['name']; 
+            $u->id = $user['id'];
+            $u->name = $user['name'];
+            $u->username = $user['username'];
+            $u->email = $user['email'];
+            $u->address = $user['address']['street'] . ', ' . $user['address']['suite'] . ', ' . $user['address']['city'] . ', ' . $user['address']['zipcode'];
+            $u->phone = $user['phone'];
+            $u->website = $user['website'];
+            $u->company = $user['company']['name'];
             $u->save();
         }
         $res = Http::get('https://jsonplaceholder.typicode.com/posts');
         $posts = json_decode($res->getBody()->getContents(), true);
-        foreach($posts as $post) {
+        foreach ($posts as $post) {
             $p = new Post();
-            $p->id = $post['id']; 
-            $p->user_id = $post['userId']; 
-            $p->title = $post['title']; 
-            $p->body = $post['body']; 
+            $p->id = $post['id'];
+            $p->user_id = $post['userId'];
+            $p->title = $post['title'];
+            $p->body = $post['body'];
             $p->save();
         }
     }
