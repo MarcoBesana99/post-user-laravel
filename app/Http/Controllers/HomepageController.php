0f5c64f91d0cdaba5ec429eeb5a7e04119ad4258
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-07-02 18:43:07 +0200

    improve query by selecting only needed fields

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -6,12 +6,13 @@ use App\Models\Post;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Http\Request;
 
 class HomepageController extends Controller
 {
     public function index() {
-        $posts = Post::with(['user'])->paginate(10);
+        $posts = Post::with(['user' => function ($query) {
+            $query->select('id','name');
+        }])->paginate(10);
         $activeUsers = User::select('name')->withCount(['posts' => function (Builder $query) {
             $query->whereDate('created_at', '>=', Carbon::now()->subDays(7));
         }])->orderBy('posts_count', 'desc')->take(3)->get();
